a5ec96013865fefdb4f67bfa7963abe2d97568ca
promag-tl <tristan@promagenviro.ca>
2022-09-22 14:04:31 -0700

    finished up the tutorial

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,4 +1,4 @@
-from asyncore import read
+from asyncio import *
 from bs4 import BeautifulSoup
 
 # Open the simple html file we wrote as read only so that
@@ -9,4 +9,9 @@ with open('home.html', 'r') as html_file:
     
     # Make a new BeautifulSoup object that will contain the methods we can use for webscraping
     soup = BeautifulSoup(content, 'lxml')
-    
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+
+        print(f'{course_name} costs {course_price}')
